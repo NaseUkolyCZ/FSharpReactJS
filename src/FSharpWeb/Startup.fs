71b019ae736d01f2df65df01b6c8087fef3f2404
davidpodhola <david.podhola@naseukoly.cz>
2015-06-14 08:50:07 +0200

    Owin and SignalR added

---
+++ src/FSharpWeb/Startup.fs
@@ -0,0 +1,12 @@
+﻿namespace Server
+
+open Owin
+open Microsoft.Owin
+ 
+type Startup() =
+    member this.Configuration(app: Owin.IAppBuilder) =
+        app.MapSignalR() |> ignore
+        ()
+
+[<assembly: OwinStartup(typeof<Startup>)>]
+do ()
