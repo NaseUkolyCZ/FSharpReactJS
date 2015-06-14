8debe68d871cfb30683da6ff54d6bda8614d2be9
davidpodhola <david.podhola@naseukoly.cz>
2015-06-14 17:16:18 +0200

    SharePoint App added + accessToken send to FSharp app

--- src/FSharpWeb/Controllers/HomeController.fs
+++ src/FSharpWeb/Controllers/HomeController.fs
@@ -6,8 +6,27 @@ open System.Linq
 open System.Web
 open System.Web.Mvc
 open System.Web.Mvc.Ajax
+open System.Net
+open System.IO
+open System.Diagnostics
 
 type HomeController() =
     inherit Controller()
-    member this.Index () = this.View()
+    member this.Index () = 
+        let accessToken = this.Request.[ "accessToken" ]
+        this.Session.["accessToken"] <- accessToken
+
+        let endpointRequest : HttpWebRequest  = downcast HttpWebRequest.Create("https://naseukolyczsro.sharepoint.com/_api/web/lists")
+        endpointRequest.Method <- "GET";
+        endpointRequest.Accept <-  "application/json;odata=verbose"
+        endpointRequest.Headers.Add("Authorization", 
+          "Bearer " + accessToken );
+        let endpointResponse : HttpWebResponse = downcast endpointRequest.GetResponse()
+        let stream = endpointResponse.GetResponseStream()
+        use reader = new StreamReader(stream) 
+        let content = reader.ReadToEnd()
+        Debug.WriteLine("content")
+        Debug.WriteLine(content)
+        Debug.WriteLine("-------------------------------------------------------------------------------")
+        this.View()
 
